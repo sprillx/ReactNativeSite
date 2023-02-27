b2b20515e03c991f08653a97cdf6f577ed3403ee
brian <118747961+sprillx@users.noreply.github.com>
2023-02-27 11:45:11 -0500

    Add files via upload

---
+++ screens/DirectoryScreen.js
@@ -0,0 +1,46 @@
+import { FlatList } from "react-native-gesture-handler";
+import { Tile } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/loadingComponent";
+import { View } from "react-native";
+import { Text } from "react-native-elements";
+
+
+const DirectoryScreen = ({ navigation }) => {
+        const campsites = useSelector((state) => state.campsites);
+
+        if (campsites.isLoading) {
+                return <Loading />;
+        }
+        if (campsites.errMess) {
+                return (
+                        <View>
+                                <Text>{campsites.errMess}</Text>
+                        </View>
+                );
+        }
+
+        const renderDirectoryItem = ({ item: campsite }) => {
+                return (
+                        <Tile
+                                title={campsite.name}
+                                caption={campsite.description}
+                                featured
+                                onPress={() =>
+                                        navigation.navigate('CampsiteInfo', { campsite })
+                                }
+                                imageSrc={{ uri: baseUrl + campsite.image }}
+                        />
+                );
+        };
+        return (
+                <FlatList
+                        data={campsites.campsitesArray}
+                        renderItem={renderDirectoryItem}
+                        keyExtractor={(item) => item.id.toString()}
+                />
+        );
+};
+
+export default DirectoryScreen;
